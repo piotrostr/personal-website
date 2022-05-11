f9424e13bb397aae1f71fc91af14779ac399fca4
piotrostr <piotr.ostrowski@aol.com>
2022-05-11 19:37:10 +0200

    add stackoverflow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,6 +43,25 @@ const Home: NextPage = () => {
                       alt="medium"
                     />
                   </a>
+                  <a href="https://stackoverflow.com/users/14422054/piotr-ostrowski">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      version="1.1"
+                      id="Layer_1"
+                      x="0px"
+                      y="0px"
+                      viewBox="0 0 512 512"
+                      width="30"
+                      height="30"
+                    >
+                      <g id="XMLID_1_">
+                        <path
+                          id="XMLID_8_"
+                          d="M512,320.2V512H0V320.2h64.2v128.5h384.5V320.2H512z M95.9,351.9h320.2v64.2H95.9V351.9z M103.3,283l14-62.4   l312.8,68.9l-14,62.4L103.3,283z M139.6,154.5l27-57.7l290.4,135l-27,57.7L139.6,154.5z M496.2,180.6l-39.1,51.2L202.9,36.3   L230.9,0h28.9L496.2,180.6z"
+                        />
+                      </g>
+                    </svg>
+                  </a>
                 </div>
               </div>
               <p className="text-lg my-10 max-w-sm">
@@ -68,7 +87,7 @@ const Home: NextPage = () => {
               </div>
               <div className="mt-2">
                 Everything here is available under MIT license (including this
-                website as well), feel free to check out the code on my github.
+                website), feel free to check out the code on my github.
               </div>
               <br />
               <div className="mt-2">Best,</div>
